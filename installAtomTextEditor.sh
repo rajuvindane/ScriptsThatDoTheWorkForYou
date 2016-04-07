636372f801658ae72b942d6e8ca033d8d4b3bf1e
Raju D. Vindane <raju@hamaralinux.org>
2016-04-07 13:50:17 +0530

    Changed installation method from gdebi to dpkg as dpkg does not ask any questions

--- installAtomTextEditor.sh
+++ installAtomTextEditor.sh
@@ -1,7 +1,5 @@
-apt-get -y install gdebi
-
 wget -c https://atom-installer.github.com/v1.6.2/atom-amd64.deb?s=1459548289
 mv atom-amd64.deb\?s\=1459548289 atom-amd64.deb
-gdebi atom-amd64.deb
+dpkg -i atom-amd64.deb
 
 echo "Atom installed successfully"
