a03195df0e9bd5df991e8eb9f471b44196c7b57c
Raju Nirmala Devidas <rajuvindane@cryptolab.net>
2017-03-27 12:07:07 +0530

    Added extraction method

--- installHadoop.sh
+++ installHadoop.sh
@@ -12,5 +12,9 @@ apt-get -y install openjdk-8-jre-headless openjdk-8-jdk ssh rsync
 ##Download Hadoop
 wget -c http://www.eu.apache.org/dist/hadoop/common/stable/hadoop-2.7.3.tar.gz
 
-##
+# extract the downloaded compressed tar file
+tar xvf hadoop-2.7.3.tar.gz
+
+## Rename the extracted folder
+mv hadoop-2.7.3. hadoop
 
